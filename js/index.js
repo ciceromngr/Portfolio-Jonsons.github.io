370726837f0f3cec31e44d7ffb2c303556a5f454
PauloEduardoLucas <dudu.magnificat@gmail.com>
2020-09-15 16:11:50 -0300

    Changing the carousel to slick slider

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$('.carousel-slick').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 600,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2500,
+    
+});
